930e340e74c6eedcd211205728648a21d87638be
Chris B. Behrens <cbehrens@gmail.com>
2023-04-12 14:40:21 -0500

    Initial add

--- vars/genericbuild.groovy
+++ vars/genericbuild.groovy
@@ -1,23 +1,25 @@
-def call(Map config=[:]){
+def call(Map = config[:]){
 node {
-    stage('SCM') {
-    	echo 'Gathering code from version control'
-	git branch: '${branch}', url: 'https://github.com/FeynmanFan/JenkinsGroovy.git'
-    }
+	stage('SCM') {
+		echo 'Gathering code from version control'
+		git branch: '${branch}', url: 'https://github.com/FeynmanFan/JenkinsGroovy.git'
+	}
     stage('Build') {
-	try{
-	        echo 'Building....'
-    		sh "dotnet build " + config.target
-	        echo 'Building New Feature'
-		releasenotes(changes:"true")
-        }catch(ex){
-        	echo 'Something went wrong'
-        	echo ex.toString();
-        	currentBuild.result = 'FAILURE'
-        }
-        finally{
-        	// cleanup
-        }
+		try{
+			echo 'Building....'
+			withDotNet(sdk: "sdk6"){
+				sh 'dotnet build ConsoleApp1'
+				echo 'Building new feature'
+				releasenotes(changes: "true");
+			}
+		}catch(ex){
+			echo 'Something went wrong'
+			echo ex.toString()
+			currentBuild.result = 'FAILURE'
+		}
+		finally{
+			// cleanup
+		}
     }
     stage('Test') {
         echo 'Testing....'
